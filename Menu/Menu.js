a3938ba952f94f3f66b9b0574644caf95109b811
unknown <samantha.c.egge@gmail.com>
2019-05-15 16:20:48 -0700

    Added article open slide function. Worked more with the Menu slide in/out and window('click') code.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,7 @@ const toggleMenu = () => {
   // menu.style.display = (menu.style.display === 'none' ? 'block' : 'none');
   menu.classList.toggle('menu--open')
   menu.classList.contains('menu--open') ? TweenMax.to('.menu--open', 2, {x: 350}) : TweenMax.to('.menu', 2, {x: 0});
-
+  
   // if (menu.classList.contains('menu--open')) {
   //   menuOp
 
@@ -19,15 +19,23 @@ const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', toggleMenu);
 
 
-//// Window click closes menu
-// window.addEventListener('click', event => {
+// //// Window click closes menu
+// window.addEventListener('mouseup', event => {
 //   // event.stopPropagation();
-//   // if (menu.classList.contains('menu--open')) {
-//   //   menu.addEventListener('click', event => {
-//   //     event.stopPropagation();
-//   //   })    
+//   if (menu.classList.contains('menu--open')) {
+//     })    
 //     menu.classList.remove('menu--open');
 //   }
 // //}
 // )
 
+// window.addEventListener("mouseup", function(event) {
+//   if (
+//     event.target != menu &&
+//     event.target != menuButton &&
+//     event.target != menu > ul >li &&
+//     event.target != menu > ul
+//   ) {
+//     toggleMenu();
+//   }
+// });
