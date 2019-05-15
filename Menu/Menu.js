30692e376f4d681a7c4d2ad7f3217f4b8a9aa9ab
unknown <samantha.c.egge@gmail.com>
2019-05-15 15:30:26 -0700

    Added shadow to menu li items in css. Began working on funcationality for windowed menu close in menu.js.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,11 @@ const toggleMenu = () => {
   // menu.style.display = (menu.style.display === 'none' ? 'block' : 'none');
   menu.classList.toggle('menu--open')
   menu.classList.contains('menu--open') ? TweenMax.to('.menu--open', 2, {x: 350}) : TweenMax.to('.menu', 2, {x: 0});
+
+  // if (menu.classList.contains('menu--open')) {
+  //   menuOp
+
+  // }
 }
 
 // Create a reference to the ".menu" class
@@ -12,3 +17,17 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that 
 // calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+
+//// Window click closes menu
+// window.addEventListener('click', event => {
+//   // event.stopPropagation();
+//   // if (menu.classList.contains('menu--open')) {
+//   //   menu.addEventListener('click', event => {
+//   //     event.stopPropagation();
+//   //   })    
+//     menu.classList.remove('menu--open');
+//   }
+// //}
+// )
+
